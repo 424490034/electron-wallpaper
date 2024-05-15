9e2002e71ac79b185f8fb64458bec41c08f63651
xuli <424490034@qq.com>
2024-05-15 19:22:06 +0800

    electron-as-wallpaper test

---
+++ src/main/wallpaper.ts
@@ -0,0 +1,39 @@
+import Electron, { BrowserWindow } from 'electron';
+import { attach } from 'electron-as-wallpaper';
+
+type CreateWallpaperProps = {
+  display: Electron.Display;
+};
+
+export default async ({ display }: CreateWallpaperProps) => {
+  let wallWindow: BrowserWindow | null = null;
+  if (!wallWindow) {
+    wallWindow = new BrowserWindow({
+      opacity: 0,
+      // transparent: true,
+      frame: false,
+      fullscreen: true,
+      webPreferences: {
+        webSecurity: false,
+        devTools: process.env.NODE_ENV === 'development',
+        nodeIntegration: true,
+        nodeIntegrationInWorker: true,
+        // nodeIntegrationInSubFrames: true,
+        contextIsolation: true,
+      },
+    });
+
+    wallWindow?.setBounds(display.bounds);
+
+    await wallWindow.loadURL('https://www.baidu.com');
+
+    if (wallWindow) {
+      attach(wallWindow, {
+        transparent: true,
+        forwardKeyboardInput: true,
+        forwardMouseInput: true,
+      });
+    }
+  }
+  return wallWindow;
+};
